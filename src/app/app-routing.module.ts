aad4214f8d02b305d0ebf4d2e9d1b21d61f3cedc
Juan Andres Troconis Redondo <jatroconis4@gmail.com>
2022-04-05 01:01:12 -0500

    update Subproducts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from '../app/core/components/layout/layout.component';
 import { HomeComponent } from '../app/core/pages/home/home.component';
 
+import { SubproductComponent } from './core/pages/subproduct/subproduct.component';
 import { ProductComponent } from './core/pages/product/product.component';
 import { ProductTypeComponent } from './core/pages/product-type/product-type.component';
 import { MainCategoryComponent } from './core/pages/main-category/main-category.component';
@@ -16,7 +17,8 @@ const ROUTES : Routes = [
           { path: 'home',         component: HomeComponent },
           { path: 'category',     component: MainCategoryComponent },
           { path: 'product-type', component: ProductTypeComponent },
-          { path: 'product',      component: ProductComponent }
+          { path: 'product',      component: ProductComponent },
+          { path: 'subproduct',      component:  SubproductComponent}
     ]
   }
 ];
